2aa63cc9cdc424e7bd8789b0d3d4e960cfd402ca
Shakhobiddin11 <shakhobiddinolimjonov@gmail.com>
2026-03-20 13:53:19 +0100

    implement API fetch and render single book

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const input = document.getElementById("searchInput");
 const button = document.getElementById("searchBtn");
 const status = document.getElementById("status");
+const results = document.getElementById("results");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", async () => {
   const query = input.value.trim();
 
   if (!query) {
@@ -12,5 +13,45 @@ button.addEventListener("click", () => {
 
   status.textContent = "Loading...";
 
-  console.log("Searching for:", query);
+  try {
+    const response = await fetch(
+      `https://openlibrary.org/search.json?q=${query}`
+    );
+
+    if (!response.ok) {
+      throw new Error("API error");
+    }
+
+    const data = await response.json();
+
+    if (data.docs.length === 0) {
+      status.textContent = "Nothing found";
+      return;
+    }
+
+    status.textContent = "";
+
+    const book = data.docs[0];
+
+    const cover = book.cover_i
+      ? `https://covers.openlibrary.org/b/id/${book.cover_i}.jpg`
+      : "./assets/book.svg";
+
+    results.innerHTML = `
+      <div class="book-card">
+        <div class="book-img-row">
+          <img src="${cover}" class="book-img" alt="Book cover">
+        </div>
+
+        <div class="book-info-grid">
+          <p class="book-title">${book.title}</p>
+          <p class="book-author">${book.author_name?.join(", ") || "Unknown"}</p>
+          <p class="published-year">${book.first_publish_year || "N/A"}</p>
+        </div>
+      </div>
+    `;
+
+  } catch (error) {
+    status.textContent = "Error fetching data";
+  }
 });
